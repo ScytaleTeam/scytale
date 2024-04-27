2fd45512bde637aef03b162f03c350958daab51d
koybasimuhittin <koybasimuhittin@gmail.com>
2024-04-27 12:18:58 +0300

    add formatter and type checking

--- packages/nextjs/src/components/providers.tsx
+++ packages/nextjs/src/components/providers.tsx
@@ -1,50 +1,46 @@
-"use client"
+"use client";
 
-import "@rainbow-me/rainbowkit/styles.css"
-import {
-	darkTheme,
-	getDefaultConfig,
-	RainbowKitProvider,
-} from "@rainbow-me/rainbowkit"
-import { WagmiProvider } from "wagmi"
-import { hardhat, sepolia, scroll } from "wagmi/chains"
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
-import Header from "./header"
-import { Toaster } from "@/components/ui/toaster"
+import "@rainbow-me/rainbowkit/styles.css";
+import { darkTheme, getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { hardhat, sepolia, scroll } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import Header from "./header";
+import { Toaster } from "@/components/ui/toaster";
 
 const config = getDefaultConfig({
-	appName: "Scytale",
-	projectId: "YOUR_PROJECT_ID",
-	chains: [hardhat, sepolia, scroll],
-	ssr: true, // If your dApp uses server side rendering (SSR)
-})
+  appName: "Scytale",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [hardhat, sepolia, scroll],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<WagmiProvider config={config}>
-			<QueryClientProvider client={queryClient}>
-				<RainbowKitProvider
-					theme={darkTheme({
-						accentColor: "#D1EF70",
-						accentColorForeground: "black",
-						borderRadius: "medium",
-						fontStack: "system",
-						overlayBlur: "small",
-					})}
-				>
-					<div className="min-h-screen relative">
-						<Header />
-						<div className="flex items-center justify-center h-full w-full absolute">
-							<div className="md:px-12 px-4 w-full h-full">{children}</div>
-						</div>
-						<Toaster />
-					</div>
-				</RainbowKitProvider>
-			</QueryClientProvider>
-		</WagmiProvider>
-	)
-}
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          theme={darkTheme({
+            accentColor: "#D1EF70",
+            accentColorForeground: "black",
+            borderRadius: "medium",
+            fontStack: "system",
+            overlayBlur: "small",
+          })}
+        >
+          <div className="min-h-screen relative">
+            <Header />
+            <div className="flex items-center justify-center h-full w-full absolute">
+              <div className="md:px-12 px-4 w-full h-full">{children}</div>
+            </div>
+            <Toaster />
+          </div>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
 
-export default Providers
+export default Providers;
