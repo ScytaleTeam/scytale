1193453856a70a074624b04208652969ca4a4e0c
koybasimuhittin <koybasimuhittin@gmail.com>
2024-04-29 21:53:14 +0300

    [nextj] update

--- packages/nextjs/src/app/message/page.tsx
+++ packages/nextjs/src/app/message/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button"
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline"
 export default function Page() {
-  return <></>
+  return (
+    <div className="mt-20">
+      <Button>
+        Send <PaperAirplaneIcon className="w-6 h-6" />
+      </Button>
+    </div>
+  )
 }
