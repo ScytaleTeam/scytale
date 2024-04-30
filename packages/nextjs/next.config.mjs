798fbc8033b7afa1ef3166d746b35ed801f26177
koybasimuhittin <koybasimuhittin@gmail.com>
2024-04-30 06:54:02 +0300

    [nextj] update

--- packages/nextjs/next.config.mjs
+++ packages/nextjs/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   headers: async () => {
     return [
       {
-        source: "/*",
+        source: "/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
